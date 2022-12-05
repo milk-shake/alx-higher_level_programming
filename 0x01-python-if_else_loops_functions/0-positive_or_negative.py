762028aa55f890ef34f3413ca32f57f5afe451cd
milk-shake <joegikenye@gmail.com>
2022-12-05 15:24:22 +0300

    is zero condition

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(number, "is positive" if number > 0 else "is negative",end="\n")
+print(number, "is positive" if number > 0 else "is negative" if number < 0 else "is zero",end="\n")
